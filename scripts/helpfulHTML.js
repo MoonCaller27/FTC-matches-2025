e374490b324cb8434e37d227d730663c7b576869
MoonCaller27 <kadenrwu@gmail.com>
2025-06-29 15:06:21 -0700

    Update helpfulHTML.js

--- scripts/helpfulHTML.js
+++ scripts/helpfulHTML.js
@@ -1,33 +1,17 @@
-//Extra things I wrote that were useful in the creation of FTC Matches
-
-//thing that saves 3 lines of code when i want a simple div with class and text
-export function createDivWithClassAndText(classN, text){
-  var div = document.createElement("div");
-  div.className = classN;
-  div.appendChild(document.createTextNode(text));
-  return div;
+import React from 'react';
+export function createDivWithClassAndText({ classN, text }) {
+  return <div className={classN}>{text}</div>;
 }
-//oh no way an all-in-one solution to make a score thing
-export function createTeamDiv(colorClass, teamNumber, teamName, isBold){
-  var div = document.createElement("div");
-  div.className = colorClass + " team";
-  // var s1 = document.createElement("span");
-  // s1.textContent="#0";
-  // s1.className = "rank";
-  // div.appendChild(s1);
-  var s2 = document.createElement("span");
-  s2.textContent = teamNumber;
-  if(isBold){
-    s2.className = "bold";
-  }
-  div.appendChild(s2);
-  div.appendChild(createDivWithClassAndText("team-name", teamName));
-  return div;
+export function createTeamDiv({ colorClass, teamNumber, teamName, isBold = false }) {
+  return (
+    <div className={`${colorClass} team`}>
+      <span className={isBold ? 'bold' : undefined}>
+        {teamNumber}
+      </span>
+      {createDivWithClassAndText({ classN: 'team-name', text: teamName })}
+    </div>
+  );
 }
-//because you can't just automatically duplicate elements, they are live objects
-export function gimmeADivider(){
-  var divider = document.createElement("div");
-  divider.className = "divider";
-  return divider;
+export function gimmeADivider() {
+  return <div className="divider" />;
 }
-
