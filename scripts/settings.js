bad2c60e91dcd1761cfcd002ca7807ad953a03dd
MoonCaller27 <kadenrwu@gmail.com>
2025-07-12 18:28:34 -0700

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'https://esm.sh/react';
 
 export function OptionsForm() {
   const [username, setUsername] = useState('');
